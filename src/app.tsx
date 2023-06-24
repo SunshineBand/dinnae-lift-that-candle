7d55801acec1cd10f65cb5f94f215f320bc23fc8
SunshineBand <kyledchenoweth@gmail.com>
2023-06-24 21:22:37 +1000

    feat: added tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,22 @@
 import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
+      <h1 class="flex justify-center">Hello world</h1>
+      <section>
+        <article>
+          <p>How many times must one count?</p>
+          <p>They must count {count} times</p>
+        </article>
+      </section>
+      <section>
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          but the count is {count}
         </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      </section>
     </>
   )
 }
